ae93ffb50fb0607e230553a4620c1aed8d503bed
Bati Zhao <zhaobati@gmail.com>
2021-06-25 09:42:37 +0800

    Update token

--- stalber-api/stalber-terrace-api/src/main/java/me/batizhao/terrace/config/ThirdPartyClientConfig.java
+++ stalber-api/stalber-terrace-api/src/main/java/me/batizhao/terrace/config/ThirdPartyClientConfig.java
@@ -37,7 +37,7 @@ public class ThirdPartyClientConfig {
                             // not available when building PRs...
                             // https://docs.travis-ci.com/user/environment-variables/#defining-encrypted-variables-in-travisyml
                             "Authorization",
-                            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXJyZW50VGltZU1pbGxpcyI6IjE2MjQ1MjczMjY3NDMiLCJleHAiOjE2MjQ1NzA1MjYsImFjY291bnQiOiJqc29hIn0.FkEuTWKnMXPLgcFib8W1XdshhLYh7SLXvy0wP9dupqE")
+                            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXJyZW50VGltZU1pbGxpcyI6IjE2MjQ2MTQ4ODEzNDQiLCJleHAiOjE2MjQ2NTgwODEsImFjY291bnQiOiJqc29hIn0.pCkS4Je_rgHMPPfH3Z44VU-H56mXs8yGfTYFBXSSFS4")
                     .header("Content-Type", "application/json");
                 })
                 .target(TerraceApi.class, thirdPartyServiceProperties.getTerraceServiceUrl());
