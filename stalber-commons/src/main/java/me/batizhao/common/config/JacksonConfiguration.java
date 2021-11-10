75f727be98fe2d9c3544e97559bb4d9b3244f69d
Bati Zhao <zhaobati@gmail.com>
2021-11-10 10:37:25 +0800

    修复日期序列化的问题。

--- stalber-commons/src/main/java/me/batizhao/common/config/JacksonConfiguration.java
+++ stalber-commons/src/main/java/me/batizhao/common/config/JacksonConfiguration.java
@@ -1,6 +1,7 @@
 package me.batizhao.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -32,7 +33,7 @@ public class JacksonConfiguration {
             builder.indentOutput(true);
             builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
             builder.simpleDateFormat(NORM_DATETIME_PATTERN);
-            builder.modules(new PecadoJavaTimeModule());
+            builder.modules(new JavaTimeModule());
         };
     }
 
