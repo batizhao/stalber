1aa3376bd9375a8a2b51c0c6a0c4ce94db93206b
魏伟思 <280104706@qq.com>
2022-03-18 18:49:44 +0800

    初始化对象

---
+++ stalber-app/src/main/java/me/batizhao/app/view/InitApp.java
@@ -0,0 +1,40 @@
+package me.batizhao.app.view;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import me.batizhao.terrace.vo.InitProcessDefView;
+
+import java.io.Serializable;
+
+/**
+ * <p> 应用初始化对象 </p>
+ *
+ * @author wws
+ * @since 2022-03-15 12:15
+ */
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@Schema(description = "应用初始化对象")
+public class InitApp implements Serializable {
+
+    /**
+     * 名称
+     */
+    @Schema(description="名称")
+    private String name;
+
+    /**
+     * 编码
+     */
+    @Schema(description="编码")
+    private String code;
+
+    /**
+     * 流程初始对象
+     */
+    @Schema(description="流程初始对象")
+    private InitProcessDefView process;
+}
